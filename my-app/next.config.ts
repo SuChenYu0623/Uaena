078a4839c93d39e6bb93ac4343f7a136d6aa60b7
SuChenYu0623 <F110154121@nkust.edu.tw>
2025-01-25 01:56:15 +0800

    chore: XXX

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,23 +1,7 @@
-import { NextConfig } from "next";
-
-const isProd = process.env.NODE_ENV === 'production';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // 使用靜態導出
-  assetPrefix: '/Uaena/',
-  basePath: '/Uaena',
-  trailingSlash: true,
-
   
-  // output: 'export', // 使用靜態導出
-  // assetPrefix: isProd ? '/Uaena/' : '',
-  // basePath: isProd ? '/Uaena' : '',
-  // trailingSlash: true,
-
-  // webpack(config) {
-  //   config.output.publicPath = isProd ? '/Uaena/' : '';
-  //   return config;
-  // },
 };
 
 export default nextConfig;
