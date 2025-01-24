1bf22475ff724b34a5f64f222e78cdc00f1873c2
SuChenYu0623 <F110154121@nkust.edu.tw>
2025-01-24 21:45:09 +0800

    chore: 設定github page

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',      // 將 Next.js 設置為靜態導出
+  basePath: '/Uaena',     // 設定 GitHub Pages 的子目錄
+  assetPrefix: '/Uaena/', // 設定靜態資源的前綴路徑
+  trailingSlash: true,   // 保證路徑以斜線結尾
 };
 
 export default nextConfig;
