00deb0b65a7c019b27349cf8a664a7a68ad25fb3
chris <chris@funmula.com>
2025-03-19 19:38:48 +0800

    feat: 決定好wiki區塊分配

--- my-app/app/wiki/page.tsx
+++ my-app/app/wiki/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-      <div>
-        <div>wiki page</div>
-      </div>
+    <div style={{ display: 'flex' }}>
+      <div className="wiki-side-container">列表</div>
+      <div className="wiki-content-container">內容</div>
+    </div>
   );
 }
